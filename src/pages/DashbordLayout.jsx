5972fdc11ced17787e27ed1a067ee486aac5681e
chantuff <chan.tuff@yahoo.com>
2024-02-08 14:20:47 -0700

    initial commit

---
+++ src/pages/DashbordLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashbordLayout = () => {
+  return (
+    <h1>DashbordLayout Page</h1>
+  )
+}
+
+export default DashbordLayout
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>job <span>tracking</span> app</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit placeat consequuntur iusto corporis laborum dolore vero iste, quasi iure quod nam hic magnam repudiandae eius optio commodi molestiae, reiciendis ad!</p>
+        <Link to="/register" className='btn register-link'>
+          Register
+        </Link>
+        <Link to="/login" className='btn login-link'>
+          Login / Demo User
+        </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;

24aa51f576452acaee7a25d67dd4d732e11c3a9e
chantuff <chan.tuff@yahoo.com>
2024-02-08 16:21:30 -0700

    Finished login, error and register pages

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,25 +1,32 @@
 import styled from "styled-components";
 import Wrapper from "../assets/wrappers/LandingPage";
 import main from "../assets/images/main.svg";
-import logo from "../assets/images/logo.svg";
 import { Link } from "react-router-dom";
+import { Logo } from "../components";
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobify" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
-          <h1>job <span>tracking</span> app</h1>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit placeat consequuntur iusto corporis laborum dolore vero iste, quasi iure quod nam hic magnam repudiandae eius optio commodi molestiae, reiciendis ad!</p>
-        <Link to="/register" className='btn register-link'>
-          Register
-        </Link>
-        <Link to="/login" className='btn login-link'>
-          Login / Demo User
-        </Link>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit
+            placeat consequuntur iusto corporis laborum dolore vero iste, quasi
+            iure quod nam hic magnam repudiandae eius optio commodi molestiae,
+            reiciendis ad!
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn login-link">
+            Login / Demo User
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
